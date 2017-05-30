a8234849d6dcfb578e7fa05cdade438881239635
Taylor Rohrich <taylorrohrich@gmail.com>
2017-05-30 15:59:08 -0400

    Updates Homescreen

--- app/src/components/welcome.js
+++ app/src/components/welcome.js
@@ -16,6 +16,7 @@ import Bundesliga_logo from './Bundesliga_logo.png';
 import EPL_logo from './EPL_logo.png';
 import Ligue_1_logo from './Ligue_1_logo.png';
 import Serie_A_logo from './Serie_A_logo.png';
+import Sampler from './sampler.png';
 class Welcome extends Component {
   render() {
     return (
@@ -33,15 +34,35 @@ class Welcome extends Component {
     </div>
     </MuiThemeProvider>
         <div className = "welcome-sub"> 
-            Access all the latest Soccer updates at your fingertips. <br/> Click a League above to begin.
         </div>
+        <img src = {Sampler} />
+        <MuiThemeProvider>
+          <div>
+          <AppBar style={{
+      width: '100%',
+      margin: '0 auto',
+      backgroundColor: "lightblue",
+      textAlign: "Center",
+      textColor: "white",
+    }} iconElementLeft = {<div></div>} title = {<div>Access all the latest Soccer updates at your fingertips.</div>}/>
+    <AppBar style={{
+      width: '100%',
+      margin: '0 auto',
+      backgroundColor: "lightgrey",
+      textAlign: "Center",
+      textColor: "white",
+    }} iconElementLeft = {<div></div>} title = {<div>Click a league below to begin.</div>}/>
+    
+    </div>
+    </MuiThemeProvider>
         <div className = "welcomeimages">
-<img className = "welcomeimages" src = {La_liga_logo} />
-<img className = "welcomeimages" src = {Bundesliga_logo} />
-<img className = "welcomeimages" src = {EPL_logo} />
+<Link to="EPL"><img className = "welcomeimages" src = {EPL_logo} /></Link>
+<Link to="Bundesliga"><img className = "welcomeimages" src = {Bundesliga_logo} /></Link>
+<Link to="Serie A"><img className = "welcomeimages" src = {Serie_A_logo} /></Link>
 <br/>
-<img className = "welcomeimages" src = {Ligue_1_logo} />
-<img className = "welcomeimages" src = {Serie_A_logo} />
+<Link to="/La%20Liga" ><img className = "welcomeimages" src = {La_liga_logo} /></Link>
+<Link to="Ligue 1"><img className = "welcomeimages" src = {Ligue_1_logo} /></Link>
+
 </div>
       </div>
     );
