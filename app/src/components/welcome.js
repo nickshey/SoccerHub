cc9f649835f2910b719a65c4d7a0652f94b16f86
Taylor Rohrich <taylorrohrich@gmail.com>
2017-05-30 14:35:48 -0400

    logos added

--- app/src/components/welcome.js
+++ app/src/components/welcome.js
@@ -11,6 +11,11 @@ import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
 import welcomeimg from './example.jpg'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import La_liga_logo from './La_Liga_logo.png';
+import Bundesliga_logo from './Bundesliga_logo.png';
+import EPL_logo from './EPL_logo.png';
+import Ligue_1_logo from './Ligue_1_logo.png';
+import Serie_A_logo from './Serie_A_logo.png';
 class Welcome extends Component {
   render() {
     return (
@@ -30,7 +35,14 @@ class Welcome extends Component {
         <div className = "welcome-sub"> 
             Access all the latest Soccer updates at your fingertips. <br/> Click a League above to begin.
         </div>
-<img className = "exampleimg" src = {welcomeimg} />
+        <div className = "welcomeimages">
+<img className = "welcomeimages" src = {La_liga_logo} />
+<img className = "welcomeimages" src = {Bundesliga_logo} />
+<img className = "welcomeimages" src = {EPL_logo} />
+<br/>
+<img className = "welcomeimages" src = {Ligue_1_logo} />
+<img className = "welcomeimages" src = {Serie_A_logo} />
+</div>
       </div>
     );
   }
