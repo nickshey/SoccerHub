e8ed5fc7ae8716442b440eec71e7312d5ac14962
Nojan Sheybani <nojan@d-172-25-137-238.dhcp.virginia.edu>
2017-12-01 13:07:43 -0500

    updating EPL api:

--- app/src/components/EPL.js
+++ app/src/components/EPL.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Timeline } from 'react-twitter-widgets'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import Paper from 'material-ui/Paper';
-import Menu from 'material-ui/Menu';
-import MenuItem from 'material-ui/MenuItem';
-import FlatButton from 'material-ui/FlatButton';
-import ReactTooltip from 'react-tooltip'
+import React, { Component } from "react";
+import axios from "axios";
+import { Timeline } from "react-twitter-widgets";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import Paper from "material-ui/Paper";
+import Menu from "material-ui/Menu";
+import MenuItem from "material-ui/MenuItem";
+import FlatButton from "material-ui/FlatButton";
+import ReactTooltip from "react-tooltip";
 
 class EPL extends Component {
-
-    state = {
+  state = {
     details: [],
     loading: true,
-    error: null,
-  }
+    error: null
+  };
 
   componentDidMount() {
-    axios.get("https://api.football-data.org/v1/competitions/426/leagueTable", { 
-          headers: { "X-Auth-Token" : "d552225d25d546c58e79bd0d09eedb5e"}
-        })
-          .then(res => {
-               let details = res.data
-                this.setState({
-                  details,
-                  loading: false,
-                  error: null
-                });
+    axios
+      .get("https://api.football-data.org/v1/competitions/445/leagueTable", {
+        headers: { "X-Auth-Token": "d552225d25d546c58e79bd0d09eedb5e" }
       })
-          .catch(err => {
-                this.setState({
-                  loading: false,
-                  error: err
-                });
+      .then(res => {
+        let details = res.data;
+        this.setState({
+          details,
+          loading: false,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          loading: false,
+          error: err
+        });
       });
   }
 
@@ -42,24 +42,20 @@ class EPL extends Component {
   }
 
   renderError() {
-    return (
-      <div>
-        Something went wrong: {this.state.error.message}
-      </div>
-    );
+    return <div>Something went wrong: {this.state.error.message}</div>;
   }
 
   renderDetails() {
     const { error, details } = this.state;
-    if(error) {
+    if (error) {
       return this.renderError();
     }
 
     return (
       <div>
-        <ReactTooltip place="bottom"/>
-        <tr> 
-          <th data-tip="Position">#</th> 
+        <ReactTooltip place="bottom" />
+        <tr>
+          <th data-tip="Position">#</th>
           <th data-tip="Team">Team</th>
           <th data-tip="Matches Played">MP</th>
           <th data-tip="Wins">W</th>
@@ -70,10 +66,13 @@ class EPL extends Component {
           <th data-tip="Goal Difference">GD</th>
           <th data-tip="Points">PTS</th>
         </tr>
-        {this.state.details.standing.map((standing) =>
-          <tr key = {standing.teamName}>
+        {this.state.details.standing.map(standing => (
+          <tr key={standing.teamName}>
             <td>{standing.position}</td>
-            <td><img className="crests" src = {standing.crestURI}></img> {standing.teamName}</td>
+            <td>
+              <img className="crests" src={standing.crestURI} />{" "}
+              {standing.teamName}
+            </td>
             <td>{standing.playedGames}</td>
             <td>{standing.wins}</td>
             <td>{standing.draws}</td>
@@ -83,50 +82,50 @@ class EPL extends Component {
             <td>{standing.goalDifference}</td>
             <td>{standing.points}</td>
           </tr>
-        )}
+        ))}
       </div>
     );
   }
-
 
   render() {
     const { loading } = this.state;
     return (
       <div>
-        <div >
+        <div>
           <MuiThemeProvider>
-                <AppBar style={{
-                    width: '100%',
-                    margin: '0 auto',
-                    backgroundColor: "lightgrey",
-                    textAlign: "Left",
-                    textColor: "white", }} 
-                    iconElementLeft = {<div></div>} 
-                    title = {"EPL League Table"}
-                  />
+            <AppBar
+              style={{
+                width: "100%",
+                margin: "0 auto",
+                backgroundColor: "lightgrey",
+                textAlign: "Left",
+                textColor: "white"
+              }}
+              iconElementLeft={<div />}
+              title={"EPL League Table"}
+            />
           </MuiThemeProvider>
         </div>
-        <p className = "App-intro"></p>
+        <p className="App-intro" />
         <div className="leftHalf">
           {loading ? this.renderLoading() : this.renderDetails()}
         </div>
         <div className="rightHalf">
-            <Timeline
-              dataSource={{
-                sourceType: 'profile',
-                screenName: 'premierleague'
-              }}
-              options={{
-                username: 'premierleague',
-                height: '770',
-                quantity: '1'
-              }}
-        />
+          <Timeline
+            dataSource={{
+              sourceType: "profile",
+              screenName: "premierleague"
+            }}
+            options={{
+              username: "premierleague",
+              height: "770",
+              quantity: "1"
+            }}
+          />
         </div>
-        </div>
+      </div>
     );
   }
 }
-
 
 export default EPL;
